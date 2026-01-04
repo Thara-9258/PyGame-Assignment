06d877d155d4dc3ad699b4acb68c37b6f7eb165f
Thara-9258 <1udhayakusun@hdsb.ca>
2026-01-04 09:52:14 -0500

    Start of Character State

--- assignment.py
+++ assignment.py
@@ -55,21 +55,35 @@ window = pygame.display.set_mode((windowWidth, windowHeight))
 clock = pygame.time.Clock()  #will allow us to set framerate
 
 #*********COLOR*************
+
 GREEN = (0,255,0)
 RED = (128, 22, 13)
 BLACK = (0,0,0)
 WHITE = (250,250,250)
 DUST = (146, 173, 171)
-#***********BG**************
+
+#********** GAME STATES ******
+
+state = "HOME"
+
+#***********IMAGES**************
 
 HomePageBG2 = pygame.image.load ("images/HomePageBG2.png")
 InstructionsButton = pygame.image.load ("images/InstructionsButton.png")
-scaled_image1 = pygame.transform.scale(InstructionsButton, (280,180))
 CharacterButton = pygame.image.load ("images/CharactersButton.png")
-scaled_image2 = pygame.transform.scale(CharacterButton, (304,274))
 PlayButton = pygame.image.load("images/PlayButton.png")
-scaled_image3 = pygame.transform.scale(PlayButton, (280,180))
+InstructionsBG = pygame.image.load("images/InstructionsBG.png")
+CharactersBG = pygame.image.load("images/CharactersBG.png")
+ElevenChar = pygame.image.load("images/ElevenFront.png")
 
+#***********SCALED IMAGES**************
+
+scaled_image1 = pygame.transform.scale(InstructionsButton, (280,180))
+scaled_image2 = pygame.transform.scale(CharacterButton, (304,274))
+scaled_image3 = pygame.transform.scale(PlayButton, (280,180))
+scaled_image4 = pygame.transform.scale(InstructionsBG, (1280, 720))
+scaled_image5 = pygame.transform.scale(CharactersBG, (1280, 720))
+scaled_image6 = pygame.transform.scale(ElevenChar, (225,330))
 
 #*********Fonts************
 
@@ -85,27 +99,30 @@ home_button = [
     pygame.Rect(500,220, scaled_image3.get_width(), scaled_image3.get_height())
     
 ]
-
 back_button = pygame.Rect(30, 30, 150, 60)
 
 # *********GAME LOOP**********
+
 Running = True
 while Running:
+
     # *********EVENTS**********
+
     mx, my = pygame.mouse.get_pos()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            Running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
    
     #**********MOUSE/KEYBOARD******
+
             if state == "HOME":
                 if home_button[0].collidepoint(mx, my): #Checks if mouse clicks button
                     state = "INSTRUCTIONS"
                 elif home_button[1].collidepoint(mx, my):
-                    state = "CHARACTER"
+                    state = "CHARACTERS"
                 elif home_button[2].collidepoint(mx, my):
                     state = "PLAY"
 
@@ -113,26 +130,35 @@ while Running:
                 if back_button.collidepoint(mx, my):
                     state = "HOME"
 
-    # *********GAME LOGIC**********
-    Gamename = "The Upside Run"
-    #PUT YOUR GAME LOGIN HERE FOR EACH GAMESTATE
+    #*********** STATES ****************
 
-    #*************BG**************
+    if state == "INSTRUCTIONS":
+        window.fill(BLACK)
+        window.blit(scaled_image4, (0,0))
+        
+    elif state == "CHARACTERS":
+        window.fill(BLACK)
+        window.blit(scaled_image5, (0,0))
+        window.blit(ElevenChar, (235,160))
 
-    window.fill((0,0,0))
-    window.blit(HomePageBG2,(40,23))
-    window.blit(scaled_image1,(500,35))
-    window.blit(scaled_image2,(488,90))
-    window.blit(scaled_image3, (500,220))
+    #if state == "PlAY":
+    elif state == "HOME":
+        Gamename = "The Upside Run"
+        window.fill((0,0,0))
+        window.blit(HomePageBG2,(40,23))
+        window.blit(scaled_image1,(500,35))
+        window.blit(scaled_image2,(488,90))
+        window.blit(scaled_image3, (500,220))
+        renderText2 =font2.render(Gamename, 1, pygame.Color(DUST))
+        window.blit(renderText2, (426,380))
+
     
-    # *********DRAW THE FRAME**********
-
-    renderText2 =font2.render(Gamename, 1, pygame.Color(DUST))
-    window.blit(renderText2, (426,380))
+    # *********GAME LOGIC**********
 
     #PUT YOUR DRAWING, IMAGE PLACEMENT, BLIT ETC.. COMMANDS HERE FOR EACH GAMESTATE
 
     # *********SHOW THE FRAME TO THE USER**********
+
     pygame.display.flip()
     clock.tick(60) #Force frame rate to 60fps or lower
 
